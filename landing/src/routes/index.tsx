f759909e1c81f5b6629dd87c52688510d6e6b4fe
benrobo <alumonabenaiah71@gmail.com>
2025-10-30 19:25:22 +0100

    added vercel analytics

--- landing/src/routes/index.tsx
+++ landing/src/routes/index.tsx
@@ -7,6 +7,7 @@ import HowItWorks from "../components/sections/HowItWorks";
 import Demo from "../components/sections/Demo";
 import CTA from "../components/sections/CTA";
 import ChangelogWidget from "../components/ChangelogWidget";
+import { Analytics } from "@vercel/analytics/next";
 
 export const Route = createFileRoute("/")({
   component: App,
@@ -70,6 +71,7 @@ function App() {
       <Demo />
       <CTA />
       <ChangelogWidget />
+      <Analytics />
     </div>
   );
 }
